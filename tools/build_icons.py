d544429a7b38346cf297af903ce3f8f8d260dfe9
David Krummenacher <dave@kruda.ch>
2022-08-20 09:42:55 +0200

    Tried out some stuff. Need to handle paths.

--- tools/build_icons.py
+++ tools/build_icons.py
@@ -1,23 +1,32 @@
 import os
 from scour import scour
 
-os.makedirs('../icons/', exist_ok = True)
 
-options = scour.parse_args([
-    '--set-precision=5',
-    '--create-groups',
-    '--strip-xml-prolog',
-    '--remove-descriptive-elements',
-    '--enable-comment-stripping',
-    '--enable-viewboxing',
-    '--no-line-breaks',
-    '--strip-xml-space',
-    '--enable-id-stripping',
-    '--shorten-ids'
-])
+def build():
+    os.makedirs('./icons/', exist_ok=True)
 
-# TODO: Do this for each icon
-options.infilename = '../source/icons/gonzago.svg'
-options.outfilename = '../icons/gonzago.svg'
-(input, output) = scour.getInOut(options)
-scour.start(options, input, output)
+    options = scour.parse_args([
+        '--set-precision=5',
+        '--create-groups',
+        '--strip-xml-prolog',
+        '--remove-descriptive-elements',
+        '--enable-comment-stripping',
+        '--enable-viewboxing',
+        '--no-line-breaks',
+        '--strip-xml-space',
+        '--enable-id-stripping',
+        '--shorten-ids'
+    ])
+
+    # TODO: Do this for each icon
+    options.infilename = './source/icons/gonzago.svg'
+    options.outfilename = './icons/gonzago.svg'
+    (input, output) = scour.getInOut(options)
+    scour.start(options, input, output)
+
+
+if __name__ == '__main__':
+    print('=================')
+    print('Building icons...')
+    print('=================')
+    build()
