40bfffff2aa67d6ef268ba7e9f89cfaff8a6149b
David Krummenacher <dave@kruda.ch>
2022-08-20 13:36:11 +0200

    Better setup bat.

--- tools/setup_environment.bat
+++ tools/setup_environment.bat
@@ -19,17 +19,22 @@ echo %toolsDir%
 echo Looking for virtual environment...
 if exist ..\env\Scripts\activate.bat (
     echo Virtual environment already exists. Activating environment...
+    echo on
+    cmd /k ..\env\Scripts\activate.bat
 ) else (
     echo Virtual environment does not exists. Creating environment...
     python -m venv ..\env
-    if errorlevel 1 goto errorNoPython
-    pip install -r ..\requirements.txt
-    if errorlevel 1 goto errorNoPython
-    echo Environment created and setup. Activating environment...
-)
 
-echo on
-cmd /k ..\env\Scripts\activate.bat
+    echo Environment created. Activating environment and settings up dependancies...
+    ..\env\Scripts\activate.bat
+    pip install -r ..\requirements.txt
+
+    echo Setup complete.
+    echo.
+
+    echo on
+    cmd /k
+)
 
 goto stop
 ::exit 0
