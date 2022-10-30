adfe1e0b7fc1f848c55efab5869d406021289582
David Krummenacher <dave@kruda.ch>
2022-10-30 23:51:14 +0100

    Minor changes.

---
+++ tools/init.bat
@@ -0,0 +1,41 @@
+:: Initial checks and setup
+:: ========================
+@echo off
+
+:: Look for python installation
+for /F "tokens=* USEBACKQ" %%i in (`python --version 3`) do set PYTHON_VERSION=%%i
+if %ERRORLEVEL% neq 0 (
+    echo Error^: Python not found! Please ensure Python is installed and added to environment.
+    exit /B %ERRORLEVEL%
+)
+echo %PYTHON_VERSION% found!
+
+:: Look for existing virtual environment and activate it
+if exist ..\env\Scripts\activate.bat (
+    echo Activating environment...
+    call ..\env\Scripts\activate.bat
+    if %ERRORLEVEL% neq 0 echo Error^: Couldn't activate virtual environment.
+    exit /B %ERRORLEVEL%
+)
+
+:: Create virtual environment if it didn't exist
+echo Virtual environment doesn't exist. Creating environment...
+python -m venv ..\env
+if %ERRORLEVEL% neq 0 (
+    echo Error^: Couldn't create virtual environment.
+    exit /B %ERRORLEVEL%
+)
+
+:: Activate virtual environment
+call ..\env\Scripts\activate.bat
+if %ERRORLEVEL% neq 0 (
+    echo Error^: Couldn't activate virtual environment.
+    exit /B %ERRORLEVEL%
+)
+
+:: Install dependancies from requirements.txt
+echo Installing requirements...
+echo.
+pip install -r ..\requirements.txt
+if %ERRORLEVEL% neq 0 echo Error^: Couldn't install requirements to virtual environment.
+exit /B %ERRORLEVEL%
--- tools/run.bat
+++ tools/run.bat
@@ -1,58 +1,25 @@
+:: Open a virtual environment or run a script in it
+:: ================================================
 @echo off
 
 :: Initial checks and setup
-call :init
+call init.bat
 echo.
 
-:: If has no argument keep window open for user,
-:: otherwise try to execute script from argument
+:: If has no argument keep window open for user
 if [%~1]==[] (
     if %ERRORLEVEL% neq 0 (
         pause
         exit /B %ERRORLEVEL%
     )
     cmd /k
-) else (
-    if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-    python %~f1
-)
-exit /B %ERRORLEVEL%
-
-:init
-:: Look for python installation
-for /F "tokens=* USEBACKQ" %%i in (`python --version 3`) do set PYTHON_VERSION=%%i
-if %ERRORLEVEL% neq 0 (
-    echo Error^: Python not found! Please ensure Python is installed and added to environment.
-    exit /B %ERRORLEVEL%
-)
-echo %PYTHON_VERSION% found!
-
-:: Look for existing virtual environment and activate it
-if exist ..\env\Scripts\activate.bat (
-    echo Activating environment...
-    call ..\env\Scripts\activate.bat
-    if %ERRORLEVEL% neq 0 echo Error^: Couldn't activate virtual environment.
     exit /B %ERRORLEVEL%
 )
 
-:: Create virtual environment if it didn't exist
-echo Virtual environment doesn't exist. Creating environment...
-python -m venv ..\env
-if %ERRORLEVEL% neq 0 (
-    echo Error^: Couldn't create virtual environment.
-    exit /B %ERRORLEVEL%
-)
+:: Otherwise try to execute script from argument
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 
-:: Activate virtual environment
-call ..\env\Scripts\activate.bat
-if %ERRORLEVEL% neq 0 (
-    echo Error^: Couldn't activate virtual environment.
-    exit /B %ERRORLEVEL%
-)
-
-:: Install dependancies from requirements.txt
-echo Installing requirements...
+echo Running script %~1
 echo.
-pip install -r ..\requirements.txt
-if %ERRORLEVEL% neq 0 echo Error^: Couldn't install requirements to virtual environment.
+python %~f1
 exit /B %ERRORLEVEL%
