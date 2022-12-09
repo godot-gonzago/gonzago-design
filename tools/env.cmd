657bf41c9172065cca91e78819abb81295bd6cfc
David Krummenacher <dave@kruda.ch>
2022-12-09 14:14:16 +0100

    This is stupid and unnecessairy.

--- tools/env.cmd
+++ tools/env.cmd
@@ -1,75 +1,57 @@
-:: Open a virtual python environment or run a script in it
-:: =======================================================
+:: Checks and setup for virtual environment
+:: ========================================
 @echo off
 
-echo GONZAGO DESIGN TOOLS
-echo ====================
+:: Setup
+:setup
+call :check_python
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
+call :create
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
+call :activate
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
+call :install_requirements
+exit /B %ERRORLEVEL%
 
-:: Virtual python environment handling
-:: -----------------------------------
-:: Look for python installation
+:: Check for python installation
+:check_python
 for /F "tokens=* USEBACKQ" %%i in (`python --version 3`) do set PYTHON_VERSION=%%i
 if %ERRORLEVEL% neq 0 (
     echo Error: Python not found!
     echo Please ensure Python is installed and added to system environment.
-    goto eof
+    exit /B %ERRORLEVEL%
 )
 echo %PYTHON_VERSION% found!
+exit /B 0
 
 :: Look for existing virtual environment and activate it
+:activate
 if exist ..\env\Scripts\activate.bat (
     echo Activating virtual environment...
     call ..\env\Scripts\activate.bat
     if %ERRORLEVEL% neq 0 (
         echo Error: Couldn't activate virtual environment.
     )
-    goto eof
+    exit /B %ERRORLEVEL%
 )
-
-:: Create virtual environment if it didn't exist
 echo Virtual environment doesn't exist.
+exit /B 2
+
+:: Create and activate virtual environment
+:create
 echo Creating virtual environment...
 python -m venv ..\env
 if %ERRORLEVEL% neq 0 (
     echo Error: Couldn't create virtual environment.
-    goto eof
 )
-
-:: Activate virtual environment
-call ..\env\Scripts\activate.bat
-if %ERRORLEVEL% neq 0 (
-    echo Error: Couldn't activate virtual environment.
-    goto eof
-)
+exit /B %ERRORLEVEL%
 
 :: Install dependancies from requirements.txt
+:install_requirements
 echo Installing requirements...
 echo.
 pip install -r ..\requirements.txt
 if %ERRORLEVEL% neq 0 (
     echo Error: Couldn't install requirements to virtual environment.
 )
-
-:eof
-echo.
-
-:: Script and user input handling
-:: ------------------------------
-:: If has no argument keep window open for user
-if [%~1]==[] (
-    if %ERRORLEVEL% neq 0 (
-        pause
-        exit /B %ERRORLEVEL%
-    )
-    cmd /k
-    exit /B %ERRORLEVEL%
-)
-
-:: Otherwise try to execute script from argument
-if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-
-echo Running script %~1
-echo.
-python %~f1
-echo.
 exit /B %ERRORLEVEL%
