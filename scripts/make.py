dd0a8db2b79dd99bda46d7850fb276bf4cc0de82
David Krummenacher <dave@kruda.ch>
2022-12-09 22:48:08 +0100

    More filecaching.

--- scripts/make.py
+++ scripts/make.py
@@ -1,9 +1,12 @@
 import os
 import yaml
 import hashlib
-from pathlib import PurePath
+import pathlib
+from pathlib import Path
 
-source_path = PurePath(__file__).parent.joinpath("../source")
+root_path = Path(__file__).parent.parent
+cache_path = root_path.joinpath('.cache/files.yaml')
+source_path = root_path.joinpath('source')
 
 # A utility function that can be used in your code
 def compute_md5(file_path):
@@ -13,11 +16,22 @@ def compute_md5(file_path):
             md5.update(chunk)
     return md5.hexdigest()
 
-if __name__ == '__main__':
-    print('Gathering files')
-    print('===============')
 
-    files_dict = {}
+def load_cache_from_file():
+    if not cache_path.exists():
+        return {}
+    with open(cache_path) as file:
+        return yaml.full_load(file)
+
+
+def save_cache_to_file(cache):
+    if not cache_path.parent.exists():
+        cache_path.parent.mkdir(parents = True, exist_ok = True)
+    with open(cache_path, 'w+') as file:
+        yaml.dump(cache, file)
+
+def gather_file_cache():
+    cache = {}
     for (root, dirs, files) in os.walk(source_path):
         for file in files:
             #file.endswith()
@@ -26,14 +40,52 @@ if __name__ == '__main__':
             rel_path = str(file_path.relative_to(source_path))
 
             statinfo = os.stat(file_path)
-            files_dict[rel_path] = {
-                "size": statinfo.st_size,
-                "lmod": statinfo.st_mtime,
-                "hash": compute_md5(file_path)
+            cache[rel_path] = {
+                'lmod': statinfo.st_mtime,
+                'size': statinfo.st_size,
+                'hash': compute_md5(file_path)
             }
 
-    print(yaml.dump(files_dict))
+    return cache
 
-    #for key in files_dict:
-    #    print(key)
-    #    print(files_dict[key])
+def diff_file_cache():
+    old_cache = load_cache_from_file()
+    current_cache = gather_file_cache()
+
+    new = []
+    changed = []
+    deleted = list(old_cache.keys())
+
+    for rel_path in current_cache:
+        if not rel_path in old_cache:
+            new.append(rel_path)
+            continue
+
+        # TODO: Handle removed files?
+        deleted.remove(rel_path)
+
+        old_file_info = old_cache[rel_path]
+        current_file_info = current_cache[rel_path]
+
+        # TODO: this can be integratet into the gathering process for optimization because me might don't
+        # need to calculate md5 hash!
+        if old_file_info['lmod'] == current_file_info['lmod']:
+            continue
+        if old_file_info['hash'] == current_file_info['hash']:
+            continue
+        changed.append(rel_path)
+
+    print('New files:')
+    print(new)
+    print('Changed files:')
+    print(changed)
+    print('Deleted files:')
+    print(deleted)
+
+    save_cache_to_file(current_cache)
+
+if __name__ == '__main__':
+    print('Gathering files')
+    print('===============')
+
+    diff_file_cache()
