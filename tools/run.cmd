554e20b3f00b42e5e6d9d52e02735683ffc964a6
David Krummenacher <dave@kruda.ch>
2022-12-18 20:02:08 +0100

    More cleanup.

--- tools/run.cmd
+++ tools/run.cmd
@@ -18,22 +18,23 @@ if [%1]==[] (
 )
 
 :: ...otherwise run scripts mode
-if %ERRORLEVEL% neq 0 exit /b 1
+:mode_script
+    if %ERRORLEVEL% neq 0 exit /b 1
 
-call :echo_header "Script runner"
-call :environment_activate > nul
-if %ERRORLEVEL% neq 0 exit /b 1
+    call :echo_header "Script runner"
+    call :environment_activate > nul
+    if %ERRORLEVEL% neq 0 exit /b 1
 
-for %%x in (%*) do (
-    echo Running script %%~x
-    echo.
-    python %%~fx
-    echo.
-)
+    for %%x in (%*) do (
+        echo Running script %%~x
+        echo.
+        python %%~fx
+        echo.
+    )
 
-call :environment_deactivate > nul
-if %ERRORLEVEL% neq 0 exit /b 1
-exit /b 0
+    call :environment_deactivate > nul
+    if %ERRORLEVEL% neq 0 exit /b 1
+    exit /b 0
 
 
 :: Common functions
@@ -45,6 +46,14 @@ exit /b 0
     echo ====================
     if not [%1]==[] echo %~1:
     echo.
+    exit /b 0
+
+:echo_info
+    echo [97m[46mINFO:[0m %~1
+    exit /b 0
+
+:echo_success
+    echo [97m[42mSUCCESS:[0m %~1
     exit /b 0
 
 :echo_warning
@@ -85,8 +94,7 @@ exit /b 0
     echo Installing pip tools...
     call :environment_call "pip install pip-tools"
     if %ERRORLEVEL% neq 0 exit /b 1
-    echo Installed pip tools!
-    exit /b 0
+    call :echo_success "Installed pip tools!" & exit /b 0
 
 :: Look for python installation.
 :python_exists
@@ -95,7 +103,7 @@ exit /b 0
     for /F "tokens=* USEBACKQ" %%i in (`python --version 3`) do set PYTHON_VERSION=%%i
     if %ERRORLEVEL% neq 0 call :echo_error "Python not found!" & exit /b 1
     endlocal & set PYTHON_VERSION=%PYTHON_VERSION%
-    echo %PYTHON_VERSION% found! & exit /b 0
+    call :echo_success "%PYTHON_VERSION% found!" & exit /b 0
 
 :: Look for existing virtual environment.
 :environment_exists
@@ -103,7 +111,7 @@ exit /b 0
     if not exist .\.venv call :echo_error "Virtual environment doesn't exist." & exit /b 1
     if not exist .\.venv\Scripts\activate.bat call :echo_error "Virtual environment doesn't exist." & exit /b 1
     if not exist .\.venv\Scripts\deactivate.bat call :echo_error "Virtual environment doesn't exist." & exit /b 1
-    echo Virtual environment found! & exit /b 0
+    call :echo_success "Virtual environment found!" & exit /b 0
 
 :: Create virtual environment.
 :environment_create
@@ -111,7 +119,7 @@ exit /b 0
     ::python -m venv --prompt "Gonzago" --upgrade-deps .\.venv
     python -m venv --prompt "Gonzago" .\.venv
     if %ERRORLEVEL% neq 0 call :echo_error "Failed to create virtual environment." & exit /b 1
-    echo Virtual environment created! & exit /b 0
+    call :echo_success "Virtual environment created!" & exit /b 0
 
 :: Remove virtual environment.
 :environment_remove
@@ -131,16 +139,16 @@ exit /b 0
     if not exist .\.venv\Scripts\activate.bat call :echo_error "Failed to activate virtual environment!" & exit /b 1
     call .\.venv\Scripts\activate.bat
     if %ERRORLEVEL% neq 0 call :echo_error "Failed to activate virtual environment!" & exit /b 1
-    echo Virtual environment activated! & exit /b 0
+    call :echo_success "Virtual environment activated!" & exit /b 0
 
 :: Dectivating virtual environment.
 :environment_deactivate
     echo Deactivating environment...
-    if not defined VIRTUAL_ENV echo Virtual environment already deactivated! & exit /b 0
+    if not defined VIRTUAL_ENV call :echo_success "Virtual environment already deactivated!" & exit /b 0
     if not exist .\.venv\Scripts\deactivate.bat call :echo_error "Failed to deactivate virtual environment!" & exit /b 1
     call .\.venv\Scripts\deactivate.bat
     if %ERRORLEVEL% neq 0 call :echo_error "Failed to deactivate virtual environment!" & exit /b 1
-    echo Virtual environment deactivated! & exit /b 0
+    call :echo_success "Virtual environment deactivated!" & exit /b 0
 
 :environment_call
     setlocal
@@ -161,7 +169,7 @@ exit /b 0
 :requirements_exists
     echo Looking for requirements.txt...
     if not exist .\requirements.txt call :echo_error "requirements.txt not found!" & exit /b 1
-    echo Found requirements.txt! & exit /b 0
+    call :echo_success "Found requirements.txt!" & exit /b 0
 
 :: Install dependencies from requirements.txt.
 :requirements_install
@@ -171,10 +179,9 @@ exit /b 0
     if %ERRORLEVEL% neq 0 exit /b 1
 
     call :environment_call "pip install -r .\requirements.txt"
-    if %ERRORLEVEL% neq 0 exit /b 1
+    if %ERRORLEVEL% neq 0 call :echo_error "Failed to install dependencies from requirements.txt!" & exit /b 1
 
-    echo Installed dependencies from requirements.txt!
-    exit /b 0
+    call :echo_success "Installed dependencies from requirements.txt!" & exit /b 0
 
 
 :: ----------
@@ -223,6 +230,7 @@ exit /b 0
     exit /b %1
 
 :menu_main
+    setlocal
     call :echo_header "Main menu"
     echo [1] Asset tools
     echo [2] Developer tools
@@ -238,6 +246,7 @@ exit /b 0
     exit /b 0
 
 :menu_asset_tools
+    setlocal
     call :echo_header "Asset tools"
     echo [1] Build all
     echo [2] Build editor icons
@@ -257,6 +266,7 @@ exit /b 0
     exit /b 0
 
 :menu_dev_tools
+    setlocal
     call :echo_header "Developer tools"
     :: python.exe -m pip install --upgrade pip
     :: TODO: https://www.activestate.com/products/python/pip-tools/
@@ -283,6 +293,7 @@ exit /b 0
 :: Menu functions
 :: --------------
 :enter_virtual_environment
+    setlocal
     call :echo_header "Virtual environment"
 
     echo Entered virtual environment at
