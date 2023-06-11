b27637a52594a0264214316e57a5a7223e00e1a2
David Krummenacher <dave@kruda.ch>
2023-06-11 21:58:54 +0200

    Merp.

--- source/theme_project/analyze_default_theme.gd
+++ source/theme_project/analyze_default_theme.gd
@@ -3,22 +3,32 @@ extends EditorScript
 
 
 func _run() -> void:
-    var theme_data := {}
+    var theme_data: Dictionary = {}
     var theme := ThemeDB.get_default_theme()
     for data_type in Theme.DATA_TYPE_MAX:
         var types := theme.get_theme_item_type_list(data_type)
         for type in types:
             var names := theme.get_theme_item_list(data_type, type)
             for name in names:
-                var key: Variant = theme.get_theme_item(data_type, name, type)
-                var entries: Array[String]
-                if not theme_data.has(key):
-                    entries = []
-                    theme_data[key] = entries
+                var theme_key: Variant = theme.get_theme_item(data_type, name, type)
+                var theme_entries: Dictionary
+                if not theme_data.has(theme_key):
+                    theme_entries = {}
+                    theme_data[theme_key] = theme_entries
                 else:
-                    entries = theme_data[key] as Array[String]
-                entries.append("%d, %s, %s" % [data_type, name, type])
-    for key in theme_data.keys():
-        print("%s:" % str(key))
-        var entries := theme_data[key] as Array[String]
-        print(";\n".join(entries))
+                    theme_entries = theme_data[theme_key] as Dictionary
+                var entries: Array[String]
+                if not theme_entries.has(name):
+                    entries = []
+                    theme_entries[name] = entries
+                else:
+                    entries = theme_entries[name] as Array[String]
+                entries.append(type)
+    for theme_key in theme_data.keys():
+        print("-+ %s" % theme_key)
+        var theme_entries := theme_data[theme_key] as Dictionary
+        for key in theme_entries.keys():
+            print(" |-+ %s" % key)
+            var entries := theme_entries[key] as Array[String]
+            for entry in entries:
+                print(" | |-+ %s" % entry)
