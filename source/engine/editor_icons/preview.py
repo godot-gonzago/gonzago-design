0530fab8223aae41c6cc89e831c80daa4aa31e51
David Krummenacher <dave@kruda.ch>
2023-09-02 12:05:02 +0200

    Some dummy preview setup.

--- source/engine/editor_icons/preview.py
+++ source/engine/editor_icons/preview.py
@@ -3,19 +3,53 @@ from pathlib import Path
 
 if __name__ == "__main__":
     dir = Path(__file__).parent
-    with dir.joinpath("preview.htm").open("w") as html: 
-        html.write('<!DOCTYPE html>\n')
-        html.write('<html lang="en">\n')
-        html.write('<head>\n')
-        html.write('<meta charset="UTF-8">\n')
-        html.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
-        html.write('<meta http-equiv="X-UA-Compatible" content="ie=edge">\n')
-        html.write('<title>Icons Preview</title>\n')
-        html.write('</head>\n')
-        html.write('<body bgcolor="#1d2229">\n')
-        
-        for image in dir.rglob("*.svg"):
-            html.write('<img src="%s" width="16px" height="16px">\n' % image.relative_to(dir))
+    with dir.joinpath("preview.htm").open("w") as html:
+        html.writelines([
+            '<!DOCTYPE html>\n',
+            '<html lang="en">\n',
+            '<head>\n',
+            '<meta charset="UTF-8">\n',
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n',
+            '<meta http-equiv="X-UA-Compatible" content="ie=edge">\n',
+            '<title>Icons Preview</title>\n',
+            '<style>\n',
+            '* { box-sizing: border-box; }\n',
+            '#myInput { width: 100%; }\n',
+            '#myUL { list-style-type: none; padding: 0; margin: 0; display: flex; flex-wrap: wrap; }\n',
+            '</style>\n',
+            '</head>\n',
+            '<body bgcolor="#1d2229">\n',
+            '<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Filter..">\n',
+            '<ul id="myUL">\n'
+        ])
 
-        html.write('</body>\n')
-        html.write('</html>')
+        for image in dir.rglob("*.svg"):
+            html.writelines([
+                '<li>',
+                '<img src="%s">\n' % image.relative_to(dir),
+                '</li>'
+            ])
+
+        html.writelines([
+            '</ul>\n',
+            '<script>\n',
+            'function myFunction() {\n',
+            '    var input, filter, ul, li, a, i, txtValue;\n',
+            '    input = document.getElementById("myInput");\n',
+            '    filter = input.value.toUpperCase();\n',
+            '    ul = document.getElementById("myUL");\n',
+            '    li = ul.getElementsByTagName("li");\n',
+            '    for (i = 0; i < li.length; i++) {\n',
+            '        a = li[i].getElementsByTagName("a")[0];\n',
+            '        txtValue = a.textContent || a.innerText;\n',
+            '        if (txtValue.toUpperCase().indexOf(filter) > -1) {\n',
+            '            li[i].style.display = "";\n',
+            '        } else {\n',
+            '            li[i].style.display = "none";\n',
+            '        }\n',
+            '    }\n',
+            '}\n',
+            '</script>\n',
+            '</body>\n',
+            '</html>\n'
+        ])
