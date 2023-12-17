d9968a4597bc2bc699d437399f4794b060c380fa
David Krummenacher <dave@kruda.ch>
2023-12-17 03:26:39 +0100

    Need to handle config file.

--- tools/gonzago/config.py
+++ tools/gonzago/config.py
@@ -7,3 +7,10 @@ CONFIG_FILE_PATH: Path = CONFIG_DIR_PATH.joinpath("config.toml").resolve()
 
 BASE_DIR_PATH: Path = Path(__file__).joinpath("../../..").resolve()
 SOURCE_DIR_PATH: Path = BASE_DIR_PATH.joinpath("source").resolve()
+
+# Inkscape C:\Program Files\Inkscape\bin\inkscape.exe
+# C:\Program Files\Blender Foundation\Blender 4.0\blender.exe
+#
+# Linux /usr/lib/blender
+# Win %ProgramFiles%/Inkscape/bin/inkscape.exe
+# OSX /Applications/Blender/blender.app/Contents/MacOS/blender
