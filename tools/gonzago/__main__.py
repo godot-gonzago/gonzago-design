c6125676341fb7df89bb327656efb96f8691cf04
David Krummenacher <dave@kruda.ch>
2025-03-17 05:27:28 +0100

    Implement stuff as pipelines (transformers, in, out) for more modularity. Added references.

--- tools/gonzago/__main__.py
+++ tools/gonzago/__main__.py
@@ -2,3 +2,29 @@
 from .cli.main import app
 
 app(prog_name="gonzago")
+
+# Workflow engines / Task orchestration
+# https://airflow.apache.org/
+# https://github.com/AustralianSynchrotron/Lightflow
+# https://github.com/spotify/luigi
+# https://luigi.readthedocs.io/en/stable/
+# https://docs.openwdl.org/
+# https://hevodata.com/learn/build-data-pipeline-python-guide/
+
+# Extract, Transform, and Load
+# https://medium.com/@tubelwj/five-common-python-libraries-for-etl-processing-5350a4c1bfeb
+
+# Data pipelines (THIS IS AN ARCHITECTURAL APPROACH FOR THIS!!!)
+# https://konfuzio.com/en/python-data-pipeline/#was-ist-eine-datenpipeline-in-python
+# https://docs.ploomber.io/en/latest/index.html
+# https://medium.com/@iftimiealexandru/data-pipeline-recipes-in-python-8561e07b2556
+# https://engineering.nike.com/koheesio/latest/#what-is-koheesio
+# https://docs.pydantic.dev/latest/api/experimental/#pydantic.experimental.pipeline
+# https://cgarciae.github.io/pypeln/
+# https://vmlaker.github.io/mpipe/
+# https://vmlaker.github.io/mpipe/cookbook.html | https://joblib.readthedocs.io/en/latest/#
+# https://beam.apache.org/get-started/beam-overview/ | https://github.com/apache/beam
+# https://beam.apache.org/get-started/quickstart-py/
+# https://beam.apache.org/documentation/sdks/yaml/
+
+# https://medium.com/@dkraczkowski/the-elegance-of-modular-data-processing-with-pythons-pipeline-approach-e63bec11d34f
